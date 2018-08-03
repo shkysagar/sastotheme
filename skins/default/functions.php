173981f8702f8e2ce3119d8f660031d83e856fb7
sagar shakya <sagar@otonomis.com>
2018-08-03 09:18:44 +0545

    eroor fix

--- skins/default/functions.php
+++ skins/default/functions.php
@@ -204,66 +204,6 @@ if (!function_exists('magikCreta_home_page_banner')) {
 
                 </div>
             </div>
-
-            <!-- end Slider -->
-            <script type='text/javascript'>
-                jQuery(document).ready(function () {
-                    jQuery('#rev_slider_4').show().revolution({
-                        dottedOverlay: 'none',
-                        delay: 5000,
-                        startwidth: 915,
-                        startheight: 450,
-                        hideThumbs: 200,
-                        thumbWidth: 200,
-                        thumbHeight: 50,
-                        thumbAmount: 2,
-                        navigationType: 'thumb',
-                        navigationArrows: 'solo',
-                        navigationStyle: 'round',
-                        touchenabled: 'on',
-                        onHoverStop: 'on',
-                        swipe_velocity: 0.7,
-                        swipe_min_touches: 1,
-                        swipe_max_touches: 1,
-                        drag_block_vertical: false,
-                        spinner: 'spinner0',
-                        keyboardNavigation: 'off',
-                        navigationHAlign: 'center',
-                        navigationVAlign: 'bottom',
-                        navigationHOffset: 0,
-                        navigationVOffset: 20,
-                        soloArrowLeftHalign: 'left',
-                        soloArrowLeftValign: 'center',
-                        soloArrowLeftHOffset: 20,
-                        soloArrowLeftVOffset: 0,
-                        soloArrowRightHalign: 'right',
-                        soloArrowRightValign: 'center',
-                        soloArrowRightHOffset: 20,
-                        soloArrowRightVOffset: 0,
-                        shadow: 0,
-                        fullWidth: 'on',
-                        fullScreen: 'off',
-                        stopLoop: 'off',
-                        stopAfterLoops: -1,
-                        stopAtSlide: -1,
-                        shuffle: 'off',
-                        autoHeight: 'off',
-                        forceFullWidth: 'on',
-                        fullScreenAlignForce: 'off',
-                        minFullScreenHeight: 0,
-                        hideNavDelayOnMobile: 1500,
-                        hideThumbsOnMobile: 'off',
-                        hideBulletsOnMobile: 'off',
-                        hideArrowsOnMobile: 'off',
-                        hideThumbsUnderResolution: 0,
-                        hideSliderAtLimit: 0,
-                        hideCaptionAtLimit: 0,
-                        hideAllCaptionAtLilmit: 0,
-                        startWithSlide: 0,
-                        fullScreenOffsetContainer: ''
-                    });
-                });
-            </script>
         </div>
 
         <?php
@@ -830,7 +770,7 @@ if (!function_exists('magikCreta_hotdeal_product')) {
                 <?php
                 $args = array(
                     'post_type' => 'product',
-                    'posts_per_page' => 1,
+                    'posts_per_page' => -1,
                     'meta_key' => 'hotdeal_on_home',
                     'meta_value' => 'yes',
                     'meta_query' => array(
