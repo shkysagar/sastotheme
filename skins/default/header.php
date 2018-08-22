f2edf9bcc4faf7dcf94a744ee7f2982c3ccdba8c
sagar shakya <sagar@otonomis.com>
2018-08-22 06:52:27 +0545

    ..

--- skins/default/header.php
+++ skins/default/header.php
@@ -11,74 +11,10 @@
 $MagikCreta = new MagikCreta(); ?>
 <body <?php body_class(); ?> >
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="navbar-wrapper">
-                <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Project name</a>
-                    </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#">Home</a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Account
-                                    <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <div class="navbar-content">
-                                            <div class="row">
-                                                <div class="col-md-5">
-                                                    <img src="http://placehold.it/120x120"
-                                                         alt="Alternate Text" class="img-responsive"/>
-                                                    <p class="text-center small">
-                                                        <a href="#">Change Photo</a></p>
-                                                </div>
-                                                <div class="col-md-7">
-                                                    <span>Bhaumik Patel</span>
-                                                    <p class="text-muted small">
-                                                        mail@gmail.com</p>
-                                                    <div class="divider">
-                                                    </div>
-                                                    <a href="#" class="btn btn-primary btn-sm active">View Profile</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="navbar-footer">
-                                            <div class="navbar-footer-content">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <a href="#" class="btn btn-default btn-sm">Change Passowrd</a>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <a href="http://www.jquery2dotnet.com"
-                                                           class="btn btn-default btn-sm pull-right">Sign Out</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 <hr/>
-<div id="page" class="page catalog-category-view d-none">
+<div id="page" class="page catalog-category-view">
 
     <!-- Header -->
     <header id="header">
